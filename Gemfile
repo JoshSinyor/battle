cc0d915be65945b9a155ca4740733297e2af2725
Joshua Sinyor <josh.sinyor@gmail.com>
2021-01-20 17:34:16 +0000

    Updated Gemfile, Gemfile.lock and app.rb. This reflects a temporary reversion to Shotgun and Ruby 2.7.0.

--- Gemfile
+++ Gemfile
@@ -8,6 +8,8 @@ group :test do
   gem 'rubocop'
   gem 'rubocop-rspec'
   gem 'selenium-webdriver'
+  gem 'shotgun'
   gem 'sinatra'
   gem 'sinatra-contrib'
+  gem 'webrick'
 end
--- app.rb
+++ app.rb
@@ -1,20 +1,52 @@
+#
+# frozen_string_literal: true
+#
+# require 'sinatra'
+# require 'sinatra/reloader' if development?
+#
+# set :session_secret, 'hastings'
+#
+# The Battle class is the main game.
+# class Battle < Sinatra::Base
+#   configure :development do
+#     register Sinatra::Reloader
+#   end
+#
+#   get '/' do
+#     'Testing infrastructure working!'
+#   end
+
 # frozen_string_literal: true
 
+require 'sinatra'
 require 'sinatra/base'
-require 'sinatra/reloader'
+require 'shotgun'
 
-# set :session_secret, 'hastings'
-
-# The Battle class is the main game.
+# The Battle class is the main game class.
 class Battle < Sinatra::Base
-  configure :development do
-    register Sinatra::Reloader
-  end
+  enable :sessions
 
-  get '/' do
+  get '/test_infrastructure' do
     'Testing infrastructure working!'
   end
 
-  # Start the server if ruby file executed directly.
+  get '/' do
+    @player_1_name = params[:player_1_name]
+    @player_2_name = params[:player_2_name]
+    erb(:index)
+  end
+
+  post '/names' do
+    session[:player_1_name] = params[:player_1_name]
+    session[:player_2_name] = params[:player_2_name]
+    redirect '/play'
+  end
+
+  get '/play' do
+    @player_1_name = session[:player_1_name]
+    @player_2_name = session[:player_2_name]
+    erb(:play)
+  end
+
   run! if app_file == $PROGRAM_NAME
 end
