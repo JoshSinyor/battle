126f76d1ceee1e30780f3263bdfff66609762de9
megan gosnell <megan_gosnell@aol.co.uk>
2021-01-22 16:52:44 +0000

    skinny controllers

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -4,6 +4,13 @@ require 'player'
 describe Game do
   subject(:alfred) { Player.new('Alfred') }
   subject(:bruce) { Player.new('Bruce') }
+  subject = Game.new(:alfred, :bruce)
+  it 'should initialize with two players' do
+    subject = Game.new(:alfred, :bruce)
+    expect(subject.player_1.name).to eq('alfred')
+    expect(subject.player_2.name).to eq('bruce')
+  end 
+
 
   context 'hit points' do
     it 'should receive damage' do
@@ -12,11 +19,11 @@ describe Game do
 
     it 'should receive damage' do
       expect(bruce).to receive(:receive_damage)
-      Game.new.attack(bruce)
+      subject.attack(bruce)
     end
 
     it 'should reduce when damage received' do
-      expect { Game.new.attack(bruce) }.to change { bruce.hit_points }.by(-10)
+      expect { subject.attack(bruce) }.to change { bruce.hit_points }.by(-10)
     end
   end
 end
